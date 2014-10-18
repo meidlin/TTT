e4be2543a4030b6dd359fcab8455b6d4b6e9be9b
Mike Eidlin <Mike@Mikes-MacBook-Air.local>
2014-10-17 17:52:46 -0700

    It's friday... not sure where I am... Avoid vegas at all costs #fearandloathing

--- app.js
+++ app.js
@@ -1,139 +1,144 @@
 var TTTApp = angular.module('TTTApp', ["firebase"]) //sets up the angular app 
+
 .controller('TTTController', function ($scope,$firebase){ //sets up the controller
+	var gameRef = new Firebase("https://mikettt.firebaseIO.com/databaseGameContainer");
+	$scope.remoteGameContainer = $firebase(gameRef);
 
-	$scope.remoteGameContainer = $firebase(new Firebase("https://mikettt.firebaseIO.com/databaseGameContainer")) ;
-	$scope.testString = ""; //setting var to test if the app and controller work. if it does, nothing should show up.
+//Initial cellList to set up board with ng-repeat.
 
-	$scope.cellList= [
-		{status:"empty", data: 1},
-		{status:"empty", data: 2},
-		{status:"empty", data: 4},
-		{status:"empty", data: 8},
-		{status:"empty", data: 16},
-		{status:"empty", data: 32},
-		{status:"empty", data: 64},
-		{status:"empty", data: 128},
-		{status:"empty", data: 256}
-	];
-
-//(Alternator, player data, win)logics
-	//Declerations
-	// $scope.gameContainer.counter = 0; //counter for X / O playerAlternator function.
-	var player1 = []; //create empty arrary of player1, where numbers will go
-	var player2 = []; //create empty array of player2, where numbers will go
+//Declerations for new game
 	var wins = [7,56,448,73,146,292,273,84]; //declaring win conditions
-	$scope.player1winsCounter = 0; //player 1 win counter
-	$scope.player2winsCounter = 0; //player 2 win counter
-	$scope.thePlayer1Total = 0;
-	$scope.thePlayer2Total = 0;
 
-
-// This container object is what gets synced:
-  $scope.gameContainer = {
-    cellList: $scope.cellList,
-    clickCounter: $scope.counter,
-    player1Total: $scope.thePlayer1Total,
-    player2Total: $scope.thePlayer2Total,
-    player1: player1,
-    player2: player2,
-    wins: wins
-  } ;
-
-  $scope.gameContainer.counter = 0;
-  $scope.remoteGameContainer.$bind($scope, "gameContainer");
-
-  $scope.$watch('gameContainer', function() {
-    console.log('gameContainer changed!');
+  //some firebase shit
+	gameRef.once("value", function(dataSnapshot){
+    if(!dataSnapshot.val() || dataSnapshot.val().numPlayers == 2){
+      $scope.playerNum = 0;
+    } 
+    else {
+      $scope.playerNum = 1;
+    }
+    console.log("I'm player " + $scope.playerNum);
+    // This container object is what gets synced:
+    $scope.gameContainer = {
+	    cellList: [
+        {status:"empty", data: 1},
+        {status:"empty", data: 2},
+        {status:"empty", data: 4},
+        {status:"empty", data: 8},
+        {status:"empty", data: 16},
+        {status:"empty", data: 32},
+        {status:"empty", data: 64},
+        {status:"empty", data: 128},
+        {status:"empty", data: 256}
+      ],
+	    clickCounter: $scope.counter,
+	    player1Total: 0, // sum of move values
+	    player2Total: 0,
+	    player1winsCounter: 0,
+	    player2winsCounter: 0,
+      numPlayers: $scope.playerNum + 1
+    };
+    $scope.remoteGameContainer.$bind($scope, "gameContainer");
+    // Set up everything
+    $scope.newGame();
   });
 
- //For some reason, only resets after 2 games 
-  $scope.newGame = function () {
-  		//resets board
+//For some reason, only resets after 2 games 
+	$scope.newGame = function () {
+		//for loop resetting cellList status and data
 		for(i = 0; i < 9; i++){
-		$scope.gameContainer.cellList[i].status="empty";
-		$scope.gameContainer.cellList[i].data= Math.pow(2,i);
+  		$scope.gameContainer.cellList[i].status = "empty";
+  		$scope.gameContainer.cellList[i].data = Math.pow(2,i);
 		};
-
-		console.log($scope.cellList[0]);
+		//Resets previous declerations.
+		console.log($scope.gameContainer.cellList[0]);
 		$scope.gameContainer.counter = 0;
-		$scope.gameContainer.player1 = 0;
-		$scope.gameContainer.player2 = 0;
 		$scope.gameContainer.player1Total = 0; //reset player1Total to 0
 		console.log("player1 total is " + $scope.gameContainer.player1Total); //test player1Total
 		$scope.gameContainer.player2Total = 0;
 		console.log("Play again was clicked"); //test
-		$scope.gameContainer.counter = 0;
 	};
 
 
-//the game
+//The game
 	$scope.playerAlternator= function (thisCell) { //start of alternator function which switches clicks between users
-		if (thisCell.status!="empty"){ //if statement which will check to see if the cell equals null or a value. If it doesn't equal null, BAIL! 
+		//if statement which will check to see if thisCell.status = "empty". If not, bail.
+		if (thisCell.status!="empty" || ($scope.gameContainer.counter % 2) != $scope.playerNum){ 
 			return;
 		}
-		//otherwise, proceed with the function
-		$scope.gameContainer.counter++; //each time the function repeats (via click), increment the var counter.
+//otherwise, proceed with the function
+		//each time the function repeats (via click), increment the var counter.
+		$scope.gameContainer.counter++; 
 
-		console.log("Cell was: " + thisCell.status) ; //check to see what the previous value was.
+		//check to see what the previous value was.
+		console.log("Cell was: " + thisCell.status); 
+		//Player alternator
 			if (($scope.gameContainer.counter % 2) != 0) {
-			console.log($scope.gameContainer.counter); //if counter is even after modulo, the cell's status = X. If odd, make it equal O. 
 				thisCell.status = "X";
-				//player1 data	
-				player1.push(thisCell.data); //push said number(data) into player2 array
-				console.log(player1); //test to see if this works
-
 				//for loop adding the values of player1 
-				$scope.gameContainer.player1Total = 0;
-				for (i=0; i< player1.length; i++){
-				$scope.gameContainer.player1Total += player1[i]
-				};
+				$scope.gameContainer.player1Total += thisCell.data;
 				console.log("Player 1 total: " + $scope.gameContainer.player1Total);
-
 
 				//win logic for player 1
 					for (i=0; i < wins.length; i++){
-						if (($scope.gameContainer.player1Total & $scope.gameContainer.wins[i]) == $scope.gameContainer.wins[i]) {
-
-							$scope.gameEnd = true;
-
-							$scope.player1winsCounter++;
-							console.log($scope.gameEnd);
-							console.log("Player 1 has won " + $scope.player1winsCounter + " times");
-							alert("PLAYER 1 WINS");
-
+						if (($scope.gameContainer.player1Total & wins[i]) == wins[i]) {
+							$scope.gameContainer.player1winsCounter++;
+							console.log("Player 1 has won " + $scope.gameContainer.player1winsCounter + " times");
+								//alert player 1 wins
+								swal({
+									title: "Player 1 wins!",      
+									type: "success",   
+									confirmButtonText: "Cool",
+									imageSize: "200x100" });
+							$scope.newGame();
 						};
 					}
-				
-			} else {
+			} 
+			else {
 				thisCell.status ="O";
-				//player2 data
-				console.log($scope.gameContainer.counter);
-				player2.push(thisCell.data); //push said number(data) into player2 array
-				console.log(player2); //test to see if this works
-
 				//for loop adding the values of player2
-				$scope.gameContainer.player2Total = 0;
-				for (i=0; i< player2.length; i++){
-				$scope.gameContainer.player2Total += player2[i];
-				};
+				$scope.gameContainer.player2Total += thisCell.data;
 				console.log("Player 2 total: " + $scope.gameContainer.player2Total);
 				//win logic for player 2
-					for (i=0; i < $scope.gameContainer.wins.length; i++){
-						if (($scope.gameContainer.player2Total & $scope.gameContainer.wins[i])== $scope.gameContainer.wins[i]){
+					for (i=0; i < wins.length; i++){
+						if (($scope.gameContainer.player2Total & wins[i])== wins[i]){
 							console.log("PLAYER 2 WINS");
-							$scope.player2winsCounter++;
-							console.log("Player 2 has won " + $scope.player2winsCounter + " times");
-							alert("PLAYER 2 WINS");
+							$scope.gameContainer.player2winsCounter++;
+							console.log("Player 2 has won " + $scope.gameContainer.player2winsCounter + " times");
+							swal({
+								title: "Player 2 wins!",     
+								type: "success",   
+								confirmButtonText: "Cool",
+								imageSize: "200x100" });
+							$scope.newGame();
 						};
 					}
 			}
-			//check to see cell is X or O
-			console.log("This cell is now: " + thisCell.status) ; 
+					
+				
 
+//whatever, betch
+$scope.goPlayerOne = function () {
+    swal({
+      title: "Player 1",   
+      text: "You get to go first!",
+      imageUrl: "images/player1.png",     
+      confirmButtonText: "cool"
+    });
+  };
+
+$scope.goPlayerTwo = function () {
+    swal({
+      title: "Player 2",   
+      text: "Wait for Player 1 to make their move.",
+      imageUrl: "images/player2.png",     
+      confirmButtonText: "cool"
+    });
+  };
+
+//check to see cell is X or O
+	console.log("This cell is now: " + thisCell.status) ; 
 	}
 	
 
 });
-
-//reset
-
